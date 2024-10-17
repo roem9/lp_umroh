ff69be16a7a3104893f8dfb2fe7a35ebac867d58
roem9 <muhammadrum04@gmail.com>
2024-10-17 20:45:42 +0700

    update lp webinar umroh

--- app/Views/landing_page/webinar-umroh.php
+++ app/Views/landing_page/webinar-umroh.php
@@ -134,24 +134,41 @@
                 <div class="elementor-element elementor-element-3cb8573 elementor-align-center elementor-widget elementor-widget-button" data-id="3cb8573" data-element_type="widget" data-widget_type="button.default">
                     <div class="elementor-widget-container">
                         <div class="elementor-button-wrapper">
-                            <a class="elementor-button elementor-button-link elementor-size-sm elementor-animation-grow" href="https://api.whatsapp.com/send?phone=<?= $agent['no_wa'] ?>&amp;text=Assalamualaykum...%20Mohon%20info%20grup%20webinar%20umrohnya." target="_blank">
+                            <a class="elementor-button elementor-button-link elementor-size-sm elementor-animation-grow" href="#formDaftar">
                                 <span class="elementor-button-content-wrapper">
-                                    <span class="elementor-button-icon">
-                                        <svg aria-hidden="true" class="e-font-icon-svg e-fab-whatsapp" viewbox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path>
-                                        </svg> </span>
-                                    <span class="elementor-button-text">JOIN GRUP WHATSAPP SEKARANG</span>
+                                    <span class="elementor-button-text">DAFTAR GRATIS SEKARANG</span>
                                 </span>
                             </a>
                         </div>
                     </div>
                 </div>
+                <div>
+                </div>
                 <div class="elementor-element elementor-element-be5ae28 elementor-widget elementor-widget-image" data-id="be5ae28" data-element_type="widget" data-widget_type="image.default">
-                    <div class="elementor-widget-container">
+                    <div class="elementor-widget-container" style="margin-bottom:1px">
                         <img loading="lazy" decoding="async" width="481" height="1024" data-src="<?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-481x1024.png" class="attachment-large size-large wp-image-4069 lazyload" alt="" data-srcset="<?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-481x1024.png 481w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-141x300.png 141w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-768x1636.png 768w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-721x1536.png 721w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-962x2048.png 962w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2.png 1080w" data-sizes="(max-width: 481px) 100vw, 481px" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" style="--smush-placeholder-width: 481px; --smush-placeholder-aspect-ratio: 481/1024;"><noscript><img loading="lazy" decoding="async" width="481" height="1024" src="<?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-481x1024.png" class="attachment-large size-large wp-image-4069" alt="" srcset="<?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-481x1024.png 481w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-141x300.png 141w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-768x1636.png 768w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-721x1536.png 721w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2-962x2048.png 962w, <?= base_url() ?>/public/assets/template-lp/webinar-umroh-haji/wp-content/uploads/2024/09/6-2.png 1080w" sizes="(max-width: 481px) 100vw, 481px"></noscript>
                     </div>
                 </div>
-                <div class="elementor-element elementor-element-d8260c6 elementor-align-center elementor-widget elementor-widget-button" data-id="d8260c6" data-element_type="widget" data-widget_type="button.default">
+                <form action="<?= base_url() ?>/registrasi/saveCustomer" method="post" id="formDaftar" style="margin-bottom: 100px">
+                    <input type="hidden" name="fk_id_agent" value="<?= MD5($agent['pk_id_agent']) ?>">
+                    <input type="hidden" name="fk_id_produk" value="<?= MD5($produk['pk_id_produk']) ?>">
+                    <div style="margin-bottom: 10px; text-align:center">
+                        <input type="text" name="nama_customer" placeholder="Nama Depan" style="width: 75%;" required>
+                    </div>
+                    <div style="margin-bottom: 10px; text-align:center">
+                        <input type="text" name="no_wa" placeholder="Nomor Whatsapp" style="width: 75%;" required>
+                    </div>
+                    <div style="margin-bottom: 10px; text-align:center">
+                        <input type="email" name="email" placeholder="Email UTAMA" style="width: 75%;" required>
+                    </div>
+                    <!-- <input type="text" name="nama_customer" value="">
+                                                <input type="text" name="no_wa" value="">
+                                                <input type="email" name="email" value=""> -->
+                    <div style="display:flex; justify-content:center;">
+                        <button type="submit" class="ff-btn ff-btn-submit ff-btn-md ff_btn_style" style="background-color: #ff0000; color: white">DAFTAR GRATIS SEKARANG</button>
+                    </div>
+                </form>
+                <!-- <div class="elementor-element elementor-element-d8260c6 elementor-align-center elementor-widget elementor-widget-button" data-id="d8260c6" data-element_type="widget" data-widget_type="button.default">
                     <div class="elementor-widget-container">
                         <div class="elementor-button-wrapper">
                             <a class="elementor-button elementor-button-link elementor-size-sm elementor-animation-grow" href="https://api.whatsapp.com/send?phone=<?= $agent['no_wa'] ?>&amp;text=Assalamualaykum...%20Mohon%20info%20grup%20webinar%20umrohnya." target="_blank">
@@ -165,7 +182,7 @@
                             </a>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
@@ -676,6 +693,18 @@
             return d
         }
     </script>
+
+    <?php if (session()->getFlashdata('message')): ?>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script>
+            Swal.fire({
+                title: 'Berhasil!',
+                text: '<?= session()->getFlashdata('message') ?>',
+                icon: 'success',
+                confirmButtonText: 'Tutup'
+            });
+        </script>
+    <?php endif; ?>
 
     <?php
     if (!empty($pixel)) {
